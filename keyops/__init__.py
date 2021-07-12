dc8532512959bf14d07c94809fb247508e79ccb6
CW <45475710+cheesemid@users.noreply.github.com>
2021-07-12 04:30:00 -0400

    make importing better
    allows for: `import keyops; keyops.sha256_ops.hash()`. previously this was required: `import keyops.sha256_ops; keyops.sha256_ops.create()`

--- keyops/__init__.py
+++ keyops/__init__.py
@@ -1,3 +1,18 @@
-version = "0.0.1a2"
+import importlib
+import sys
 
-from keyops import *
+
+
+
+version = "0.0.1a3"
+package_name = "keyops"
+__all__ = ["aes_key_ops", "rsa_key_ops", "sha256_ops", "totp_ops"]
+
+
+
+
+importlib.import_module(package_name)
+this = sys.modules[__name__]
+
+for mod in __all__:
+    setattr(this, mod, importlib.import_module(f"{package_name}.{mod}"))
